e0ce3078ac30b7d3945d2786c80871e63fdeea17
Stephen <nehpets0701@gmail.com>
2021-07-14 14:35:41 -0400

    recovered

---
+++ 0x00-ls/hls.h
@@ -0,0 +1,16 @@
+#ifndef HLS_H
+#define HLS_H
+
+/*includes*/
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+/*includes*/
+
+/*prototypes*/
+int count(void);
+/*prototypes*/
+#endif
