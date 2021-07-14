c5c8a44ddc6b7a88c91dde1281ee481eca231d48
Stephen <nehpets0701@gmail.com>
2021-07-14 01:25:10 -0400

    spacing

--- 0x00-ls/hls.c
+++ 0x00-ls/hls.c
@@ -9,11 +9,23 @@
 int main(int argc, char **argv)
 {
 	(void) argc;
+	int printed = 0;
+	DIR * dirp = opendir(".");
+	struct dirent * entry;
 	struct dirent *de;
+	int file_count = 0;
 	DIR *dr = opendir(".");
 
 	if (argv[1] != NULL)
 		dr = opendir(argv[1]);
+
+	while ((entry = readdir(dirp)) != NULL)
+	{
+		if (entry->d_type == DT_REG)
+		{
+			file_count++;
+		}
+	}
 
 	if (dr == NULL)
 	{
@@ -28,8 +40,15 @@ int main(int argc, char **argv)
 	while ((de = readdir(dr)) != NULL)
 	{
 		if (de->d_name[0] != '.')
-			printf("%s\n", de->d_name);
+		{
+			if (printed <= file_count)
+				printf("%s ", de->d_name);
+			else
+				printf("%s\n", de->d_name);
+		}
+		printed ++;
 	}
+	printed = 0;
 	closedir(dr);
 	return (0);
 }
