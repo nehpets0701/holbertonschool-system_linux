72baaefd328308f2471b1f0149b6a4b48b461c56
Stephen <nehpets0701@gmail.com>
2022-02-08 13:22:29 -0500

    2 and 3

---
+++ 0x0C-sockets/3-server.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+
+/**
+ * main-opens and closes a socket after connection
+ * Return: 0/1
+ */
+int main(void)
+{
+	int sock, newSock, clientSize;
+	struct sockaddr_in addr, client;
+	char buffer[1024] = {0};
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+		return (EXIT_FAILURE);
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(12345);
+
+	if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+		return (EXIT_FAILURE);
+	listen(sock, 3);
+	clientSize = sizeof(client);
+	newSock = accept(sock, (struct sockaddr *)&client, (socklen_t *)&clientSize);
+	if (newSock < 0)
+		return (EXIT_FAILURE);
+
+	printf("Client connected: %s\n", inet_ntoa(client.sin_addr));
+
+	if (recv(newSock, buffer, 1024, 0) < 0)
+		return (EXIT_FAILURE);
+	printf("Message received: \"%s\"\n", buffer);
+	close(sock);
+	close(newSock);
+	return (0);
+}
